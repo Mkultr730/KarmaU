88ccd3f5015ffff8e1475aa51a6148ed72940068
Martin Molinares Araujo <memolinares@uninorte.edu.co>
2020-09-29 13:53:58 -0500

    first commit

---
+++ app/src/main/java/com/memolinares/karma_androidpf/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.memolinares.karma_androidpf
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
