aa42ab3187a1e9e115900dc75ea14422839c2ad1
lpotte <lpotte@uninorte.edu.co>
2020-10-18 22:04:55 -0500

    we can see the favors list

--- app/src/main/java/com/memolinares/karma_androidpf/viewModel/FavorViewModel.kt
+++ app/src/main/java/com/memolinares/karma_androidpf/viewModel/FavorViewModel.kt
@@ -1,10 +1,14 @@
 package com.memolinares.karma_androidpf.viewModel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.memolinares.karma_androidpf.model.Favor
+import com.memolinares.karma_androidpf.model.User
 import com.memolinares.karma_androidpf.repository.FavorRepository
 
 class FavorViewModel: ViewModel() {
+    var userLiveData = MutableLiveData<User>()
+
     var favorRepository = FavorRepository()
 
     fun askFavor(favor: Favor) = favorRepository.askFavor(favor)
