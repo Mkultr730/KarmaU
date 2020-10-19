8f4afd6a62af45b69f6bd60385df40806e4816fa
Martin Molinares <memolinares@uninorte.edu.co>
2020-10-19 02:20:45 -0500

    another one v4

--- app/src/main/java/com/memolinares/karma_androidpf/repository/LoginRepository.kt
+++ app/src/main/java/com/memolinares/karma_androidpf/repository/LoginRepository.kt
@@ -4,6 +4,9 @@ package com.memolinares.karma_androidpf.repository
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.memolinares.karma_androidpf.model.User
 import com.google.firebase.ktx.Firebase
@@ -21,4 +24,16 @@ class LoginRepository  {
     fun getCurrentUser() = auth.currentUser
 
     fun savedUser(user: User) = database.child(FirebaseAuth.getInstance().uid.toString()).setValue(user)
+
+    fun getUser() = database.child(FirebaseAuth.getInstance().uid.toString()).addListenerForSingleValueEvent(object :
+        ValueEventListener {
+        override fun onCancelled(error: DatabaseError) {
+            TODO("Not yet implemented")
+        }
+
+        override fun onDataChange(snapshot: DataSnapshot) {
+            TODO("Not yet implemented")
+        }
+
+    })
 }
